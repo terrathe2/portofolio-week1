ecb8fc561a0d947dc2c2ed65bc5545620befa6ae
redhaputra <putra.redha@gmail.com>
2017-10-28 23:25:35 +0700

    changing mongo db url

--- to-do-fancy-api/server/models/todo.js
+++ to-do-fancy-api/server/models/todo.js
@@ -1,6 +1,7 @@
 const helper = require('../helper/helper')
 const mongoose = require('mongoose')
-const url = "mongodb://admin:admin@localhost:27017/todo" // with mongoDB role authenticate
+// const url = "mongodb://admin:admin@localhost:27017/todo" // with mongoDB role authenticate
+const url = "mongodb://terrathe2:terrathe2@ds237815.mlab.com:37815/portofolio"
 mongoose.connect(url, helper.mongoAuth)
 
 const schema = new mongoose.Schema({
